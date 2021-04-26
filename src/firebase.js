bb9f05f959a8e00603d109ec185de6d2ce0ced4c
digomattar21 <digomattar@gmail.com>
2021-04-26 15:47:02 -0300

    little fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import firebase from "firebase";
 const firebaseConfig = {
     apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
     authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+    databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
     projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
     storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
     messagingSenderId: `${process.env.REACT_APP_FIREBASE_MSG_SEND_ID}`,
     appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
     measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
   };
-
+  
 const fireBaseApp = firebase.initializeApp(firebaseConfig);
 const db = fireBaseApp.firestore();
 const auth = firebase.auth();
